0b51d26d06e07b9415947af69d9cd76e9fe0cfd2
Xakep <xakep@grib.pw>
2023-02-04 20:17:08 +0300

    new file

---
+++ 01_Intro_Prog/03/task_2.py
@@ -0,0 +1,13 @@
+numb = [2, 5, 13, 7, 6, 4]
+size = 6
+summ = 0
+avg = 0
+index =0
+while index < size:
+    summ = summ+ numb[index]
+    index = index + 1
+    
+avg = summ / size
+print(avg)
+    
+ 
