c74accbbde6c66f4680eee089c20d21b5ec5533b
Xaakkeep <xaakkeep@gmail.com>
2023-02-09 12:57:39 +0300

    fix lessons 02 new

---
+++ 02_Git/02/main.py
@@ -0,0 +1,14 @@
+def Hello(text):
+    print(f'Hello World {text}')
+
+def max(a,b):
+    if a>b:
+        print(a)
+    else:
+        print(b)
+
+def summa(a,b):
+    print(f'{a}+{b}')
+
+def hello_text(name):
+    print(f'Hello {name}')
---
+++ 02_Git/02/readme.md
@@ -0,0 +1,7 @@
+# commit
+
+Commit 1 master + conf  
+
+Commit 2 master + conf 
+
+Commit 3 master + conf
