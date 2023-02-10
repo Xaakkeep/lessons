871ba6084d3806da8335a986da1f2a7167e4311a
Xaakkeep <xaakkeep@gmail.com>
2023-02-10 12:45:31 +0300

    add div

--- 03_Program_C#/01/Program.cs
+++ 03_Program_C#/01/Program.cs
@@ -2,10 +2,12 @@
 string username = Console.ReadLine();
 Console.WriteLine("Hello, ");
 Console.WriteLine(username);
-
+Console.WriteLine("Sum:");
 int A = 3;
 int B = 5;
 Console.WriteLine(A+B);
-
-
+Console.WriteLine("Div: ");
+double divA = 12;
+double divB = 5;
+Console.WriteLine(divA / divB);
 
