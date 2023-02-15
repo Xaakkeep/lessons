d78c5fe887da8119badb5741949c626e7b5706c7
Xaakkeep <xaakkeep@gmail.com>
2023-02-15 15:24:00 +0300

    add homework 03 lesson 03

---
+++ 03_Program_C#/03/Program.cs
@@ -0,0 +1,16 @@
+﻿void task10()
+{
+    int i;
+    Console.Write("Введите число N: ");
+    string task10_N = Console.ReadLine()!;
+    Console.Write("Введите номер числа который вы хотите узнать: ");
+    int.TryParse(Console.ReadLine()!, out i);
+    i -= 1;
+    int[] array = new int[task10_N.Length];
+    
+    array[i] = (int)char.GetNumericValue(task10_N[i]);
+    Console.WriteLine(array[i]);
+
+
+}
+task10();
