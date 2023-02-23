fd2d18bc00352c75e34938981d1e2494481a0f41
Xaakkeep <xaakkeep@gmail.com>
2023-02-24 01:17:04 +0300

    add homework 03 lesson 05

---
+++ 03_Program_C#/05/Program.cs
@@ -0,0 +1,46 @@
+﻿int TakeConsoleInt(string str = "Введите число N: ")
+{
+    int _num;
+    Console.Write(str);
+    int.TryParse(Console.ReadLine()!, out _num);
+    return _num;
+}
+
+/* 
+
+*/
+
+void task30()
+{
+
+}
+
+/* 
+
+*/
+
+void task31()
+{
+
+}
+
+void task()
+{
+    int i;
+    Console.Write($"Выберите задание: \nЗадача 25\nЗадача 27\nЗадача 29\n");
+    int.TryParse(Console.ReadLine()!, out i);
+    if (i == 25)
+    {
+        task25();
+    }
+    if (i == 27)
+    {
+        task27();
+    }
+    if (i == 29)
+    {
+        task29();
+    }
+
+}
+task();
