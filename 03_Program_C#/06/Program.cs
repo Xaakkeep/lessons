236bdeb5a72020c6a1c9455bdac23445d42668b3
Xaakkeep <xaakkeep@gmail.com>
2023-03-05 13:56:04 +0300

    add task 41

--- 03_Program_C#/06/Program.cs
+++ 03_Program_C#/06/Program.cs
@@ -1,51 +1,58 @@
 ﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-*/
+0, 7, 8, -2, -2 -> 2
 
-void task40()
-{
-    Console.WriteLine("Task 40");
-    int[] array = RandomArray(InputText("Size: "), InputText("Min: "), InputText("Max: "));
-    PrintArray(array);
-}
-
-/*
-
+1, -7, 567, 89, 223-> 3
 */
 
 void task41()
 {
     Console.WriteLine("Task 41");
+    int[] array = RandomArray();
+    PrintArray(array);
+    array = Console.ReadLine()!.Split(' ', ',').Where(i => int.TryParse(i, out _)).Select(int.Parse).ToArray();
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    PrintArray(array);
+    Console.WriteLine($"Количество чисел больше 0 - {count}");
+         
+    
 }
 
 /*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 */
 
-void task42()
+void task43()
 {
-    Console.WriteLine("Task 42");
+    Console.WriteLine("Task 43");
+    
 }
 
 void task()
 {
-    Console.Write("Выбираите задачу\nЗадача 40\nЗадача 41\nЗадача 42\n");
+    Console.Write("Выбираите задачу\nЗадача 41\nЗадача 43\n");
     int i = InputText("");
-    if (i == 40)
-    {
-        task40();
-    }
     if (i == 41)
     {
         task41();
     }
-    if (i == 42)
+    if (i == 43)
     {
-        task42();
+        task43();
     }
 }
 
-int[] RandomArray(int size, int min, int max)
+int[] RandomArray(int size = 3, int min = 1, int max = 10)
 {
     int[] array = new int[size];
     for (int i = 0; i < size; i++)
@@ -55,13 +62,11 @@ int[] RandomArray(int size, int min, int max)
     return array;
 }
 
-void PrintArray(Array arr)
+void PrintArray(int[] arr)
 {
-    for (int i = 0; i < arr.Length; i++)
-    {
-        Console.Write($"[{arr.GetValue(i)}] ");
-    }
-    Console.WriteLine();
+    Console.Write("{");
+    Console.Write(String.Join(", ", arr));
+    Console.WriteLine("}");
 }
 
 int InputText(String text = "Введите число N: ")
