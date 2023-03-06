c49d69aa2373a26b03bc9fc49a9235bddbe31479
Xaakkeep <xaakkeep@gmail.com>
2023-03-06 10:30:22 +0300

    add task43

--- 03_Program_C#/06/Program.cs
+++ 03_Program_C#/06/Program.cs
@@ -22,8 +22,8 @@ void task41()
     }
     PrintArray(array);
     Console.WriteLine($"Количество чисел больше 0 - {count}");
-         
-    
+
+
 }
 
 /*
@@ -35,7 +35,59 @@ b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 void task43()
 {
     Console.WriteLine("Task 43");
-    
+    int coefficient = 0;
+    int constant = 1;
+    int x = 0;
+    int y = 1;
+    int line1 = 1;
+    int line2 = 2;
+
+    double[] lineData1 = InputLineData(line1);
+    double[] lineData2 = InputLineData(line2);
+
+    if (ValidateLines(lineData1, lineData2))
+    {
+        double[] coord = FindCoords(lineData1, lineData2);
+        Console.Write($"Точка пересечения уравнения y={lineData1[coefficient]}*x+{lineData1[constant]} и y={lineData2[coefficient]}*x+{lineData2[constant]} ");
+        Console.WriteLine($" имеет координаты ({coord[x]}, {coord[y]})");
+    }
+
+    double[] InputLineData(int numOfLine)
+    {
+        double[] lineData = new double[2];
+        lineData[coefficient] = InputText($"Введите коэффициент для {numOfLine} прямой >");
+        lineData[constant] = InputText($"Введите константу для {numOfLine} прямой >");
+        return lineData;
+    }
+
+    double[] FindCoords(double[] lineData1, double[] lineData2)
+    {
+        double[] coord = new double[2];
+        coord[x] = (lineData1[constant] - lineData2[constant]) / (lineData2[coefficient] - lineData1[coefficient]);
+        coord[y] = lineData1[constant] * coord[x] + lineData1[constant];
+        
+        return coord;
+    }
+
+    bool ValidateLines(double[] lineData1, double[] lineData2)
+    {
+        if (lineData1[coefficient] == lineData2[coefficient])
+        {
+            if(lineData1[constant] == lineData2[constant])
+            {
+                Console.WriteLine("Прямые совпадают");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Прямые параллельны");
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 }
 
 void task()
