db66452d8a806b6c824845d0d6635962d0542514
Xaakkeep <Xaakkeep@gmail.com>
2023-03-10 09:59:20 +0300

    add task58

--- 03_Program_C#/08/Program.cs
+++ 03_Program_C#/08/Program.cs
@@ -97,7 +97,69 @@ void task56()
 void task58()
 {
     Console.WriteLine("Task 58");
+    Console.WriteLine($"\nВведите размеры матриц и диапазон случайных значений:");
+    int m = InputText("Введите число строк 1-й матрицы: ");
+    int n = InputText("Введите число столбцов 1-й матрицы (и строк 2-й): ");
+    int p = InputText("Введите число столбцов 2-й матрицы: ");
+    int range = InputText("Введите диапазон случайных чисел: от 1 до ");
 
+    int[,] firstMartrix = new int[m, n];
+    CreateArray(firstMartrix);
+    Console.WriteLine($"\nПервая матрица:");
+    WriteArray(firstMartrix);
+
+    int[,] secomdMartrix = new int[n, p];
+    CreateArray(secomdMartrix);
+    Console.WriteLine($"\nВторая матрица:");
+    WriteArray(secomdMartrix);
+
+    int[,] resultMatrix = new int[m, p];
+
+    MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+    Console.WriteLine($"\nПроизведение первой и второй матриц:");
+    WriteArray(resultMatrix);
+
+    void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+    {
+        for (int i = 0; i < resultMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < resultMatrix.GetLength(1); j++)
+            {
+                int sum = 0;
+                for (int k = 0; k < firstMartrix.GetLength(1); k++)
+                {
+                    sum += firstMartrix[i, k] * secomdMartrix[k, j];
+                }
+                resultMatrix[i, j] = sum;
+            }
+        }
+    }
+
+    void WriteArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if (array[i, j] / 10 <= 0)
+                    Console.Write($" {array[i, j]} ");
+
+                else Console.Write($"{array[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    void CreateArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = new Random().Next(range);
+            }
+        }
+    }
 }
 
 /*
@@ -175,6 +237,8 @@ void task62()
             Console.WriteLine();
         }
     }
+
+
 }
 
 void task()
@@ -294,3 +358,4 @@ void Print3DArray(int[,,] array3D)
         Console.WriteLine();
     }
 }
+
